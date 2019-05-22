6fd71658e4f60ec0b609e41ca6f6598ff6e71bb5
linhnnle1810 <32847924+linhnnle1810@users.noreply.github.com>
2019-05-22 14:38:07 -0700

    Add files via upload

---
+++ scriptBE125.m
@@ -0,0 +1,15 @@
+%%%%%%% BENG 125 Final Project 2019 %%%%%%%%
+%%%%%%% Team Members %%%%%%%
+% Jose Zaragoza, Allen Le, Jacqueline Olness, Kokila Perera, and Linh N Le %
+% Date: 5/2019 %
+
+% Description of project:
+
+%%%%%%%%%%%%%%%%%%%%%%
+
+clear all; 
+clc;
+close all; 
+format long; 
+
+% 
